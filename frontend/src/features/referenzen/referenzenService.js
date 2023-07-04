6cfec1d11f1865ab0c0d1cf1916da133f19823b0
Roman <roman.rostock@gmail.com>
2023-07-04 16:43:31 +0200

    changed path

--- frontend/src/features/referenzen/referenzenService.js
+++ frontend/src/features/referenzen/referenzenService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = `${window.location.origin}/api/referenzen/`;
+const API_URL = 'https://weak-erin-dolphin-robe.cyclic.app/api/referenzen/';
 
 const createReferenzen = async (referenzenData, token)=>{
     const config ={
--- frontend/src/features/sprachen/sprachenService.js
+++ frontend/src/features/sprachen/sprachenService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = `${window.location.origin}/api/sprachen/`;
+const API_URL = 'https://weak-erin-dolphin-robe.cyclic.app/api/sprachen/';
 
 const createSprachen = async (sprachenData, token)=>{
     const config ={
