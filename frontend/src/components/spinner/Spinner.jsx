07dd0d08022c7163f64a999556a5a3d1b9bc0da1
Roman-e-prog <roman.rostock@googlemail.com>
2024-01-18 11:53:26 +0100

    controlled

--- frontend/src/components/spinner/Spinner.jsx
+++ frontend/src/components/spinner/Spinner.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './spinner.module.css'
+import styles from './spinner.module.scss'
 const Spinner = () => {
   return (
     <div className={styles.container}>
